20d8ae5b610f68dffe06949562a3dc4669aa99cb
Maksim Ivanov <satansdeer@gmail.com>
2024-05-05 16:51:58 +0200

    Reorder routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,13 +4,13 @@ import playlistsController from '../controllers/playlistsController';
 
 const router = express.Router();
 
+router.post('/playlists', playlistsController.createPlaylist);
+router.get('/sounds/recommended', playlistsController.getRecommendedSounds);
+
 router.post('/admin/sounds', soundsController.createSound);
 router.get('/sounds', soundsController.listSounds);
 router.get('/sounds/:id', soundsController.getSound);
 router.delete('/sounds/:id', soundsController.deleteSound);
-
-router.post('/playlists', playlistsController.createPlaylist);
-router.get('/sounds/recommended', playlistsController.getRecommendedSounds);
 
 export default router;
 
